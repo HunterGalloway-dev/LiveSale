f3364616e259220f18bd6a914c832c76333d3ee0
HunterJesus <bunterballoway01@gmail.com>
2021-04-04 18:58:17 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './Auth/sign-in/sign-in.component';
+import { SignUpComponent } from './Auth/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './Auth/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './Auth/verify-email/verify-email.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
   ],
   imports: [
     BrowserModule,
